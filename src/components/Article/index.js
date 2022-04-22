6ed2106eebbbc4040ef5ebb1f10263895da3ab19
Guilherme Augusto <guilherme.augusto0394@gmail.com>
2022-04-22 20:42:57 -0300

    Creation of component structures

---
+++ src/components/Article/index.js
@@ -0,0 +1,13 @@
+import Post from '../Post';
+
+const Article = () =>{
+    return (
+        <>
+            <h1>Article</h1>
+            <Post />
+        </>
+    );
+}
+
+
+export default Article;
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+
+
+function Header(){
+    return (
+        <h1>Header.js</h1>
+    )
+}
+
+export default Header;
