68c275ce148f3c1be90edc976f0231ae08ddf8a6
Guilherme Augusto <guilherme.augusto0394@gmail.com>
2022-04-22 21:37:46 -0300

    Styling the menu.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,25 @@
-
+import logosvg from '../../svg/logo.svg'
 
 function Header(){
+
+    const descLogo = "Essa é a logo do Blog.";
+
     return (
-        <h1>Header.js</h1>
+        <>
+            <header className="flex-space-between">
+                <div className="logo">
+                    <img src={logosvg} alt={descLogo} />
+                </div>
+                <div className="search">
+                    <input type="text" name="search" className="input-search" placeholder="Buscar artigos, podcasts, destaques..." />
+                </div>
+                <ul className="menu">
+                    <li> <a href="#" className="nav-link"> Categories </a> </li>
+                    <li> <a href="#" className="nav-link"> About </a> </li>
+                    <li> <a href="#" className="nav-link"> Contact </a> </li>
+                </ul>
+            </header>
+        </>
     )
 }
 
